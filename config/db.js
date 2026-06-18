2f45bc37f0a163097c7926f18c309688153a8718
Aashwalayan <aashwalayanjha@gmail.com>
2026-06-18 12:15:06 +0530

    day7

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const connectDB = async() =>{
+    try{
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log("MongoDB Connected Succesfully")
+    }catch (error){
+        console.log("MongoDB Connection Error:", error.message);
+        process.exit(1);
+    }
+}
+
+
+//exporting database connect function
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParse = require('body-parser')
+const bodyParse = require('body-parser');
+const connectDB = require('./config/db');
+require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParse.json());
 
-
+//database connect code
+connectDB();
 
 app.get('/user', (req, res) =>{
-    console.log('api is working')
+    console.log('api done', req.body)
     return res.send({
         'status': 200,
         'data': "api is working"
     });
-})  
+});
 
 app.listen(5000, () =>{
     console.log("server is running")
